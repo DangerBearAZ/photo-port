0788b3e0e2c1dd2e1949efc846f1fef3e69d531a
claire ogesen <claire.ogesen.co@gmail.com>
2021-06-10 17:28:13 -0700

    form is doing some stuff

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function ContactForm() {
-    return(
-    <section>
-        <h1>Contact me</h1>
-        <form id="contact=form">
-        <div>
-  <label htmlFor="name">Name:</label>
-  <input type="text" name="name" />
-</div>
-<div>
-  <label htmlFor="email">Email address:</label>
-  <input type="email" name="email" />
-</div>
-<div>
-  <label htmlFor="message">Message:</label>
-  <textarea name="message" rows="5"  />
-</div>
-<button type="submit">Submit</button>
-        </form>
-    </section>
+    //Hook that'll manage the form data
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const { name, email, message } = formState;
+
+    // handleChange function will sync the internal state of the component formState with the user input from the DOM. The onChange 
+    //event listener will ensure that the handleChange function fires whenever a keystroke is typed into the input field for name.
+    function handleChange(e) {
+        //assign the value taken from the input field in the UI with e.target.value 
+        setFormState({ ...formState, [e.target.name]: e.target.value})
+    }
+    //add a console log outside to check if it works
+    // console.log(formState);
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact=form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
     )
 }
 
